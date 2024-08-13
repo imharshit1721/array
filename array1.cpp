a70c212e63e4dc87123b6fd53886faa3904a8235
imharshit1721 <118714362+imharshit1721@users.noreply.github.com>
2024-08-13 11:51:44 +0530

    Create array1.cpp

---
+++ array1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a[5];
+    cout<<"emter the values"<<endl;
+     for(int i = 0; i<5; i++){
+          cin>>a[i];
+     }
+     
+     
+     cout<<"elements are"<<endl;
+     for(int i = 0; i <5; i++){
+         cout<<a[i]<<endl;
+     }
+ }
